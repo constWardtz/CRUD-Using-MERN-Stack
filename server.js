b11d0e22b081bdef35f5657d2b99d3b617773c62
constWardtz <edwardtutorials22@gmail.com>
2020-11-14 13:13:51 -0400

    Version 1.0.0

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ app.use('/API', Books)
 
 /* Serve static assets if in production */
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../../client/build'))
+    app.use(express.static('client/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../../client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
